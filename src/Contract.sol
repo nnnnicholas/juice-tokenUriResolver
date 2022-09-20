b95ddf7523f0a880fdcd0d4847741616f5b55c4c
nicholas <nnnnicholas@users.noreply.github.com>
2022-09-20 00:36:21 -0400

    svg fixed. send it!

--- src/Contract.sol
+++ src/Contract.sol
@@ -38,30 +38,46 @@ contract TokenUriResolver is
             projectName = projectHandles.handleOf(_projectId);
         } else {
             // Set projectName to name to 'Project #projectId'
-            projectName = string(abi.encodePacked("Project #", _projectId.toString()));
+            projectName = string(
+                abi.encodePacked("Juicebox Project # ", _projectId.toString())
+            );
         }
 
         // Get overflow
         uint256 overflow = singleTokenPaymentTerminalStore
             .currentTotalOverflowOf(_projectId, 18, 1);
 
-        string[] memory parts = new string[](3);
-        // TODO wrap in metadata JSON 
-        parts[0] = string(
+        string[] memory parts = new string[](4);
+        parts[0] = string("data:application/json;base64,");
+        parts[1] = string(
             abi.encodePacked(
-                '<svg xmlns="http://www.w3.org/2000/svg" width="300" height="400" version="1.1">',
-                '<rect width="300" height="400" stroke="black" stroke-width="6" fill="#ffaf03"/>',
-                '<text xml:space="preserve" style="font-size:42px;line-height:1;font-family:sans-serif;word-spacing:0px;white-space:pre;shape-inside:url(#rect3919)"><tspan x="19" y="50"><tspan style="font-family:Helvetica;-inkscape-font-specification:Helvetica">',
+                '{"name":"',
                 projectName,
-                "</tspan></tspan></text>",
-                '<text xml:space="preserve" style="font-size:42.6667px;line-height:1;font-family:sans-serif;word-spacing:0px;white-space:pre;shape-inside:url(#rect9222)" transform="translate(0,192.98246)"><tspan x="17.542969"y="52.328557"><tspan style="font-family:Helvetica;-inkscape-font-specification:Helvetica;fill:#ff0000">Overflow </tspan></tspan><tspan x="17"y="96"><tspan style="font-family:Helvetica;-inkscape-font-specification:Helvetica;fill:#ff0000">',
-                (overflow/10**18).toString(),
-                'ETH',
-                "</tspan></tspan></text>",
-                "</svg>"
+                '", "description":"',
+                projectName,
+                " is a project on the Juicebox Protocol. It has an overflow of ",
+                (overflow / 10**18).toString(),
+                ' ETH.", "image":"data:image/svg+xml;base64,'
             )
         );
-        string memory uri = Base64.encode(abi.encodePacked(parts[0]));
+        
+        parts[2] = Base64.encode(
+            abi.encodePacked(
+                '<svg xmlns="http://www.w3.org/2000/svg" width="300" height="400" version="1.1"> <rect width="300" height="400" stroke="black" stroke-width="6" fill="#ffaf03"/> <text xml:space="preserve" style="font-size:42px;line-height:1;font-family:sans-serif;word-spacing:0px;white-space:pre" transform="translate(0,0)"> <tspan x="19" y="52" style="font-family:Helvetica">',
+                projectName,
+                '</tspan> </text> <text xml:space="preserve" style="font-size:42.6667px;line-height:1;font-family:sans-serif;word-spacing:0px;white-space:pre" transform="translate(0,192)"> <tspan x="19" y="52" style="font-family:Helvetica;fill:#ff0000">',
+                'Overflow </tspan> <tspan x="19" y="96" style="font-family:Helvetica;-inkscape-font-specification:Helvetica;fill:#ff0000">',
+                (overflow / 10**18).toString(),
+                'ETH </tspan> </text></svg>'
+            )
+        );
+        parts[3] = string('"}');
+        string memory uri = string(
+            abi.encodePacked(
+                parts[0],
+                Base64.encode(abi.encodePacked(parts[1], parts[2], parts[3]))
+            )
+        );
         return uri;
     }
 }
