ac6ce33485a3d2e00fb1745f002f83f391b1f3c4
nicholas <nnnnicholas@users.noreply.github.com>
2022-11-17 15:50:39 -0500

    improved tests

--- test/LeftPad.t.sol
+++ test/LeftPad.t.sol
@@ -2,13 +2,9 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
-// import "../src/TokenUriResolver.sol";
 
-// import "juice-project-handles/interfaces/IJBProjectHandles.sol";
-// import "base64/base64.sol";
-
+// This function is in a separate contract so that TokenUriResolver can pass it a string memory and we can still use Array Slices (which only work on calldata)
 contract StringSlicer {
-    // This function is in a separate contract so that TokenUriResolver can pass it a string memory and we can still use Array Slices (which only work on calldata)
     function slice(
         string calldata _str,
         uint256 _start,
@@ -19,7 +15,6 @@ contract StringSlicer {
 }
 
 contract ContractTest is Test {
-
     StringSlicer slice = new StringSlicer();
 
     function testLeftPadShorter() external {
@@ -28,30 +23,36 @@ contract ContractTest is Test {
         assertEq(bytes(res).length, 10);
     }
 
-    function testLeftPadShorterUnicode() external{
+    function testLeftPadShorterUnicode() external {
         string memory str = unicode"";
         string memory res = leftPad(str, 10);
         assertEq(bytes(res).length, 10);
     }
 
-    function testLeftPadLonger() external{
+    // Ellipsis is 3 bytes
+    // Capsules' Juicebox is
+    function testUnicodeByteCount() external {
+        // console.log(bytes(unicode'…').length);
+        assertEq(bytes(unicode"…").length, 3);
+        assertEq(bytes(unicode"").length, 3);
+    }
+
+    // Anticipates 2 extra bytes bc unicode ellipsis is 3 bytes
+    function testLeftPadLonger() external {
         string memory str = "testing a very long string";
         string memory res = leftPad(str, 10);
         console.log(res);
         assertEq(bytes(res).length, 12);
     }
 
-    // Ellipsis is 3 bytes
-    function testEllipsisCount() external{
-        console.log(bytes(unicode'…').length);
-    }
-
-    function testLeftPadLongerUnicode() external{
+    // Anticipates 2 extra bytes bc unicode Capsules' Juicebox is 3 bytes
+    function testLeftPadLongerUnicode() external {
         string memory str = unicode" testing a very long string";
         string memory res = leftPad(str, 10);
-        assertEq(bytes(res).length, 10);
+        assertEq(bytes(res).length, 12);
     }
 
+    // Copied leftPad fn here bc it's an internal fn
     function leftPad(string memory str, uint256 targetLength)
         internal
         view
